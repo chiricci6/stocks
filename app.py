91ac8927449d4bf73b806125bd5d5261667772c1
chiricci6 <chiricci6@gmail.com>
2023-11-04 10:57:52 -0300

    hola

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -41,4 +42,4 @@ def agregar():
     return jsonify({"message": "Producto agregado correctamente"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
